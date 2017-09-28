06b1962fcf448de89bfab2a35393b851e79679cb
ingpoc <gurusharan.rp.gupta@gmail.com>
2017-09-28 13:36:17 +0000

    working code from rest API

--- springcassandra/src/main/java/com/cog/springcassandra/controller/UserController.java
+++ springcassandra/src/main/java/com/cog/springcassandra/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.cog.springcassandra.controller;
 
+import java.util.UUID;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -16,23 +18,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cog.springcassandra.data.User;
 import com.cog.springcassandra.model.UserForm;
 import com.cog.springcassandra.service.UserService;
+import com.datastax.driver.core.utils.UUIDs;
 import com.google.common.net.MediaType;
 
 @Component
 @Path("/user")
 public class UserController {
-	
+
 	@Autowired
 	public UserService userService;
-	
-	     @POST
-	     @Path("/create")
-	     @Consumes("application/json")
-	     @Produces("application/json")
-	    public Response create( UserForm userForm) {        
-	       User user =  userService.createUser(userForm);
-	         return Response.status(200).entity(user).build();
-	    }
-	
+
+	@POST
+	@Path("/create")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response create(UserForm userForm) {
+		UUID id = UUIDs.timeBased();
+		userForm.setId(id);
+		User user = userService.createUser(userForm);
+		return Response.status(200).entity(user).build();
+	}
 
 }
