c92079f3bbaabd26ff202c8029e57eedc81ec8a1
Boopalan <boobpalan.dev@gmail.com>
2017-10-06 06:17:07 +0000

    Updated Detail Page fix

--- springcassandra/src/main/java/com/cog/springcassandra/SpringcassandraApplication.java
+++ springcassandra/src/main/java/com/cog/springcassandra/SpringcassandraApplication.java
@@ -16,7 +16,7 @@ public class SpringcassandraApplication {
                 new SpringApplication(SpringcassandraApplication.class);
         Properties properties = new Properties();
         properties.setProperty("spring.resources.staticLocations",
-                "classpath:/static/,classpath:/static/swagger/");
+                "classpath:/static/,classpath:/static/swagger/, classpath:/static/src/");
         app.setDefaultProperties(properties);
 		SpringApplication.run(SpringcassandraApplication.class, args);
 	}
